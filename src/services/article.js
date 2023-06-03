eaf3d8ab925d37cb59b55510c90540ff5681d3e6
Sushant <srdarkseer@gmail.com>
2023-06-03 18:06:53 +0545

    removed keys

--- src/services/article.js
+++ src/services/article.js
@@ -5,12 +5,12 @@ const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    baseUrl: "",
     prepareHeaders: (headers) => {
       headers.set("X-RapidAPI-Key", rapidApiKey);
       headers.set(
-        "X-RapidAPI-Host",
-        "article-extractor-and-summarizer.p.rapidapi.com"
+        "",
+        ""
       );
 
       return headers;
